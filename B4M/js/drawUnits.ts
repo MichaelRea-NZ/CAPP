e8b6cb748e504ab9dd58c6194f13dd172c34930f
MichaelRea-NZ <58958188+MichaelRea-NZ@users.noreply.github.com>
2022-05-05 14:51:49 +1200

    Add files via upload

---
+++ B4M/js/drawUnits.ts
@@ -0,0 +1,28 @@
+import{b4mUnits} from "./units.js";
+
+
+
+
+function drawUnit(e:any, t:any, n:any, i:any) {
+    if (t.position)
+        for (var e_1 = 0; e_1 < b4mUnits.length; e_1++)
+            if (t.id === b4mUnits[e_1].id) {
+                b4mUnits[e_1].position = {
+                    x: t.position.x,
+                    y: t.position.y
+                };
+                var n_1 = grid.get(t.position);
+                n_1 && n_1.city && 'none' !== n_1.city && (n_1.control = t.army);
+                break;
+            }
+    var o = 'soviet' === t.army ? document.getElementById('soviets') : document.getElementById('germans'), r = 'full' === t.strength ? 0 : 120;
+    e.drawImage(o, 120 * t.pos, r, 120, 120, n, i, 60, 60);
+}
+function clearUnit(e:any, t:any, n:any, i:any) {
+    for (var e_2 = 0; e_2 < b4mUnits.length; e_2++)
+        if (t.id === b4mUnits[e_2].id) {
+            b4mUnits[e_2].position = null;
+            break;
+        }
+    e.clearRect(n, i, 80, 80);
+}
