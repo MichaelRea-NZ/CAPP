040aeba8d6a331126d7903082e27092fea980ba6
MichaelRea-NZ <58958188+MichaelRea-NZ@users.noreply.github.com>
2022-07-29 14:07:34 +1200

    Add files via upload

--- New sandbox/js/start.js
+++ New sandbox/js/start.js
@@ -2,54 +2,61 @@
 /* eslint semi: ["error", "never"] */
 /* eslint no-unused-vars: "error" */
 /* globals  document newGameHandler continueGameHandler continueGame highlightsCanvas */
+function addSplashListeners() {
+  // eslint-disable-line no-unused-vars
+  document
+    .getElementById("next_continue_button")
+    .addEventListener("click", startCampaignHandler, false);
 
-function addSplashListerner(){// eslint-disable-line no-unused-vars
-  document.getElementById('next_continue_button')
-  .addEventListener('click', newGameHandler, false)
-
-  document.getElementById('next_new_button')
-  .addEventListener('click', continueGameHandler, false)  
+  document
+    .getElementById("next_new_button")
+    .addEventListener("click", continueCampaignHandler, false);
 }
 
-function removeSplashListerner(){
-  document.getElementById('next_continue_button')
-  .removeEventListener('click', newGameHandler, false)
+function removeSplashListerners() {
+  document
+    .getElementById("next_continue_button")
+    .removeEventListener("click", startCampaignHandler, false);
 
-  document.getElementById('next_new_button')
-  .removeEventListener('click', continueGameHandler, false)
+  document
+    .getElementById("next_new_button")
+    .removeEventListener("click", continueCampaignHandler, false);
 }
 
-function continueCampaignHandler(){// eslint-disable-line no-unused-vars
-  //startGame()
-  removeSplashListerner()
+function continueCampaignHandler() {
+  // eslint-disable-line no-unused-vars
+  console.log("continue");
+  startCampaign();
+  removeSplashListerners();
 }
 
-function startCampaignHandler(){// eslint-disable-line no-unused-vars
-  startGame()
-  removeSplashListerner()
+function startCampaignHandler() {
+  // eslint-disable-line no-unused-vars
+  console.log("continue");
+  startCampaign();
+  removeSplashListerner();
 }
 
-function startGame() {
-  document.getElementById('next_new_button').removeEventListener('click', startGame) 
-  document.getElementById('next_continue_button').removeEventListener('click', continueGame)        
-  document.body.style.overflow = 'visible'
-  var startTitle = document.getElementById('splash_title')
-  document.body.removeChild(startTitle)
-  var startBackground = document.getElementById('splash_background')
-  document.body.removeChild(startBackground)
-  var canvasBoard = document.getElementById('board')// eslint-disable-line no-unused-vars
-  var arrowImage = document.getElementById('attack_arrow')// eslint-disable-line no-unused-vars
-  var arrowCanvas = document.getElementById('arrow_canvas')
-  var arrowsContext = arrowCanvas.getContext('2d')// eslint-disable-line no-unused-vars
-  var germanImage = document.getElementById('german_image')// eslint-disable-line no-unused-vars
-  var alliedImage = document.getElementById('allied_image')// eslint-disable-line no-unused-vars
-  var unitsCanvas = document.getElementById('units_canvas')
-  var unitsContext = unitsCanvas.getContext('2d')// eslint-disable-line no-unused-vars
-  unitsCanvas.width = document.getElementById('map_image').width
-  unitsCanvas.height = document.getElementById('map_image').height
-  arrowCanvas.width = document.getElementById('map_image').width
-  arrowCanvas.height = document.getElementById('map_image').height
-  highlightsCanvas.width = document.getElementById('map_image').width
-  highlightsCanvas.height = document.getElementById('map_image').height  
+function startCampaign() {
+  //document.getElementById('next_new_button').removeEventListener('click', startGame)
+  //document.getElementById('next_continue_button').removeEventListener('click', continueGame)
+  document.body.style.overflow = "visible";
+  var startTitle = document.getElementById("splash_title");
+  document.body.removeChild(startTitle);
+  var startBackground = document.getElementById("splash_background");
+  document.body.removeChild(startBackground);
+  var canvasBoard = document.getElementById("board"); // eslint-disable-line no-unused-vars
+  var arrowImage = document.getElementById("attack_arrow"); // eslint-disable-line no-unused-vars
+  var arrowCanvas = document.getElementById("arrow_canvas");
+  var arrowsContext = arrowCanvas.getContext("2d"); // eslint-disable-line no-unused-vars
+  var germanImage = document.getElementById("german_image"); // eslint-disable-line no-unused-vars
+  var alliedImage = document.getElementById("allied_image"); // eslint-disable-line no-unused-vars
+  var unitsCanvas = document.getElementById("units_canvas");
+  var unitsContext = unitsCanvas.getContext("2d"); // eslint-disable-line no-unused-vars
+  unitsCanvas.width = document.getElementById("map_image").width;
+  unitsCanvas.height = document.getElementById("map_image").height;
+  arrowCanvas.width = document.getElementById("map_image").width;
+  arrowCanvas.height = document.getElementById("map_image").height;
+  // highlightsCanvas.width = document.getElementById("map_image").width;
+  //highlightsCanvas.height = document.getElementById("map_image").height;
 }
-
